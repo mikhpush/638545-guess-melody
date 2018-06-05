01a9ea2b0a8abbdc8d596789300d09d53bea2dd4
Pushkov <Pushkov.mikhail@gmail.com>
2018-06-05 18:50:15 +0300

    проверка работоспособносте тестовой среды

---
+++ js/gameGenre.js
@@ -0,0 +1,136 @@
+// Игра на выбор жанра
+import {changeScreen, playAgainButton} from './utils';
+import {resultWin} from './gameResultWin';
+import {resultLossTime} from './gameResultTime';
+//import {resultLossAttepts} from './gameResultAttempts';
+
+const gameGenre = document.createElement(`section`);
+gameGenre.className = `main main--level main--level-genre`;
+gameGenre.innerHTML = `
+    <a class="play-again play-again__wrap" href="#">
+      <img class="play-again__img" src="/img/melody-logo-ginger.png" alt="logo" width="177" height="76">
+    </a>
+    <svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
+      <circle
+        cx="390" cy="390" r="370"
+        class="timer-line"
+        style="filter: url(.#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"></circle>
+
+      <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
+        <span class="timer-value-mins">05</span><!--
+        --><span class="timer-value-dots">:</span><!--
+        --><span class="timer-value-secs">00</span>
+      </div>
+    </svg>
+    <div class="main-mistakes">
+      <img class="main-mistake" src="img/wrong-answer.png" width="35" height="49">
+      <img class="main-mistake" src="img/wrong-answer.png" width="35" height="49">
+      <img class="main-mistake" src="img/wrong-answer.png" width="35" height="49">
+    </div>
+
+    <div class="main-wrap">
+      <h2 class="title">Выберите инди-рок треки</h2>
+      <form class="genre">
+        <div class="genre-answer">
+          <div class="player-wrapper">
+            <div class="player">
+              <audio></audio>
+              <button class="player-control player-control--pause"></button>
+              <div class="player-track">
+                <span class="player-status"></span>
+              </div>
+            </div>
+          </div>
+          <input type="checkbox" name="answer" value="answer-1" id="a-1">
+          <label class="genre-answer-check" for="a-1"></label>
+        </div>
+
+        <div class="genre-answer">
+          <div class="player-wrapper">
+            <div class="player">
+              <audio></audio>
+              <button class="player-control player-control--play"></button>
+              <div class="player-track">
+                <span class="player-status"></span>
+              </div>
+            </div>
+          </div>
+          <input type="checkbox" name="answer" value="answer-1" id="a-2">
+          <label class="genre-answer-check" for="a-2"></label>
+        </div>
+
+        <div class="genre-answer">
+          <div class="player-wrapper">
+            <div class="player">
+              <audio></audio>
+              <button class="player-control player-control--play"></button>
+              <div class="player-track">
+                <span class="player-status"></span>
+              </div>
+            </div>
+          </div>
+          <input type="checkbox" name="answer" value="answer-1" id="a-3">
+          <label class="genre-answer-check" for="a-3"></label>
+        </div>
+
+        <div class="genre-answer">
+          <div class="player-wrapper">
+            <div class="player">
+              <audio></audio>
+              <button class="player-control player-control--play"></button>
+              <div class="player-track">
+                <span class="player-status"></span>
+              </div>
+            </div>
+          </div>
+          <input type="checkbox" name="answer" value="answer-1" id="a-4">
+          <label class="genre-answer-check" for="a-4"></label>
+        </div>
+
+        <button class="genre-answer-send" type="submit">Ответить</button>
+      </form>
+    </div>
+    `;
+
+const genreAnswer = gameGenre.querySelector('.genre-answer-send');
+
+playAgainButton(gameGenre);
+
+genreAnswer.addEventListener(`click`, () => {
+  let randomResult = Math.floor(Math.random()*3);
+  console.log(randomResult);
+  switch (randomResult) {
+    case 0: 
+      randomResult = resultWin;
+      console.log(resultWin);
+      break;
+    case 1:
+      randomResult = resultLossTime;
+      console.log(resultLossTime);
+      break;
+    case 2: 
+      randomResult = resultLossAttepts;
+      console.log(resultLossAttepts);
+      break;
+  };
+
+  console.log(randomResult);
+  changeScreen(randomResult);
+
+});
+
+const checkboxes = gameGenre.querySelectorAll(`.genre-answer input[type="checkbox"]`);
+const submit = gameGenre.querySelector(`.genre-answer-send`);
+
+submit.disabled = true;
+
+checkboxes.forEach( function(it) { 
+  it.addEventListener(`change`, () => {
+    submit.disabled = !it.checked; 
+  })
+});
+
+
+export {gameGenre};
+
+
---
+++ js/main.js
@@ -0,0 +1,47 @@
+//дом
+
+import {mainWelcome} from './mainWelcome';
+import {changeScreen} from './utils';
+
+changeScreen(mainWelcome);
+
+/*
+
+
+
+
+
+
+
+//setInterval(findPlayAgainButton, 1000);
+
+const findReplayButton = () => {
+  const replay = mainElement.querySelector(`section`);
+  replay.querySelector(`.main-replay`).addEventListener(`click`, () => {
+  mainElement.innerHTML = ``;
+  mainElement.appendChild(mainWelcome);
+  });
+};
+
+//setInterval(findReplayButton, 1000);
+
+
+
+
+
+const disabledSubmit = () => {
+  const checkbox = document.getElementsByName(`answer`);
+  const submit = document.querySelector(`.genre-answer-send`);
+  console.log(checkbox);
+  console.log(submit); 
+  submit.disabled = true;
+
+  checkbox.forEach( function(it) { 
+      if (it.checked == true) {
+      submit.disabled = false;
+    }});
+ 
+}
+
+//etInterval(disabledSubmit, 500);
+*/
